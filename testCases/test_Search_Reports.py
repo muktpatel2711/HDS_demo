2bfd3d0155362bb56116596d154ff3c07b9e97d0
muktpatel2711 <muktpatel2711@gmail.com>
2024-09-30 17:07:23 -0400

    Added

---
+++ testCases/test_Search_Reports.py
@@ -0,0 +1,59 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import pytest
+from test_login import Test_login
+
+chrome_options=ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+firefox_options = FirefoxOptions()
+firefox_options.add_argument("--headless")
+
+
+class Test_serach_reports:
+    def __init__(self):
+        self.login_test =Test_login()
+
+    def test_search_report(self):
+        driver =self.login_test.test_login_authorization()
+        driver.implicitly_wait(50)
+        driver.find_element(By.XPATH,"//span[text()='Reports']").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//span[@class='ng-arrow-wrapper']").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH,"//span[text()='Person Report']").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH,"(//span[@class='ng-arrow-wrapper'])[2]").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//span[text()='Satsang Network']").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH,"(//span[@class='ng-arrow-wrapper'])[3]").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, "//span[text()='Center']").click()
+        time.sleep(2)
+        driver.find_element(By.XPATH,"//button[text()=' Search ']").click()
+        time.sleep(2)
+        pagr_no =driver.find_element(By.XPATH,"//span[text()='10']")
+        page_no_text=pagr_no.text
+        assert page_no_text=="10"
+        pass
+        print("A person reports works fine")
+        driver.quit()
+
+if __name__ =="__main__":
+    test_report =Test_serach_reports()
+    test_report.test_search_report()
+
+
+
+
+
---
+++ testCases/test_Search_Sampark.py
@@ -0,0 +1,47 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import pytest
+from test_login import Test_login
+
+chrome_options=ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+firefox_options = FirefoxOptions()
+firefox_options.add_argument("--headless")
+
+
+class Test_serach_sampark:
+    def __init__(self):
+        self.login_test =Test_login()
+
+    def test_search_sampark(self):
+        driver =self.login_test.test_login_authorization()
+        driver.implicitly_wait(50)
+        driver.find_element(By.XPATH,"//span[text()='Manage Sampark']").click()
+        time.sleep(3)
+        driver.find_element(By.XPATH, "//span[text()='Sampark Karyakar']").click()
+        driver.find_element(By.XPATH,"//button[text()=' Search ']").click()
+        pagr_no =driver.find_element(By.XPATH,"//span[text()='10']")
+        page_no_text=pagr_no.text
+        assert page_no_text=="10"
+        pass
+        print("seach_sampark works fine")
+        driver.quit()
+
+if __name__ =="__main__":
+    test_search =Test_serach_sampark()
+    test_search.test_search_sampark()
+
+
+
+
+
