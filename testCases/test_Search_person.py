4046195792b767f1e7cd56201bd7871508714fc1
muktpatel2711 <muktpatel2711@gmail.com>
2024-09-30 15:58:23 -0400

    Add_serach Person

--- testCases/test_Search_person.py
+++ testCases/test_Search_person.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -18,21 +19,27 @@ firefox_options = FirefoxOptions()
 firefox_options.add_argument("--headless")
 
 
-class Test_Add_person:
+class Test_serach_person:
     def __init__(self):
         self.login_test =Test_login()
 
-    def test_add_person(self):
+    def test_search_person(self):
         driver =self.login_test.test_login_authorization()
+        driver.implicitly_wait(50)
         driver.find_element(By.XPATH,"//span[text()='Manage Person']").click()
         time.sleep(3)
-        driver.find_element(By.XPATH, "//span[text()='Add Person']").click()
+        driver.find_element(By.XPATH, "//span[text()='Search Person']").click()
+        driver.find_element(By.XPATH,"//button[text()=' Search ']").click()
+        baps_id  =driver.find_element(By.XPATH,"//span[text()='1211']")
+        baps_id_text=baps_id.text
+        assert baps_id_text=="1211"
+        pass
         print("person_add")
         driver.quit()
 
 if __name__ =="__main__":
-    test_add =Test_Add_person()
-    test_add.test_add_person()
+    test_search =Test_serach_person()
+    test_search.test_search_person()
 
 
 
